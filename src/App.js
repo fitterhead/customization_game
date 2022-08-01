3190cb6430d589ba054aa22f9ea0661c360d4c4a
Morita <morita@Moritas-MacBook-Pro.local>
2022-08-01 16:45:26 -0400

    new commit

--- src/App.js
+++ src/App.js
@@ -1,272 +1,205 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import "./App.css";
+
+const total = {
+  accessories: {
+    earrings: 32,
+    glasses: 17,
+    hats: 28,
+    neckwear: 18,
+  },
+  body: 17,
+  eyes: 17,
+  hair: 73,
+  facial_hair: 17,
+  mouths: 24,
+  eyebrows: 15,
+  clothes: {
+    layer_1: 5,
+    layer_2: 5,
+    layer_3: 9,
+  },
+  noses: 1,
+};
 function App() {
-  const [count, setCount] = useState(1);
-  const increaseCount = () => setCount(count + 1);
-  // const minusCount = () => setCount(count - 1);
-  // const total = {
-  //   accessories: {
-  //     earrings: 32,
-  //     glasses: 17,
-  //     hats: 28,
-  //     neckwear: 18,
-  //   },
-  //   body: 17,
-  //   eyes: 17,
-  //   hair: 73,
-  //   facial_hair: 17,
-  //   mouths: 24,
-  //   eyebrows: 15,
-  //   clothes: {
-  //     layer_1: 5,
-  //     layer_2: 5,
-  //     layer_3: 9,
-  //   },
-  //   noses: 1,
-  // };
-
-  const total = {
-    accessories: {
-      earrings: 32,
-      glasses: 17,
-      hats: 28,
-      neckwear: 18,
-    },
-    body: 17,
-    noses: 1,
-  };
-
-  //   parentName = [acessories]
-  // total > findObject[total] > true {
-  //> [acessories [earing...](true),body:png(false)]
-  //
-  //
-  //
-  //(true)[acessories [earing...]] = true > findObject [earing]
-  //earing = false > return ( title = asscessories (for earch(earing) > earing1, 2, 3, 4) )
-  //
-  //parentName = [acessories]
-  //}
-  // (false) > body:png(false)] {
-  // if parentName !== 0
-  //  parentName = childKey
-  //
-  //
-
+  const childTitle = [];
   const totalArray = [];
-  // console.log(parentName, "1");
-  const isObject = (value) => {
-    return !!(value && typeof value === "object" && !Array.isArray(value)); // return true or false
+  const [avatarArray, setAvatarArray] = useState([
+    { avatar_location: "body", avatar_part: "body", avatar_status: 1 },
+  ]);
+
+  const addComponent = (location, part, status) => {
+    const avatarInput = {
+      avatar_location: location,
+      avatar_part: part,
+      avatar_status: status,
+    };
+    avatarArray.map((object) => {
+      if (object.avatar_part === part) {
+        object.avatar_status = status;
+      }
+      return object;
+    });
+    setAvatarArray([...avatarArray, avatarInput]);
+    console.log(avatarArray, "1");
   };
 
-  const findObject = (object) => {
+  const isObject = (value) => {
+    return !!(value && typeof value === "object" && !Array.isArray(value)); // test input if an object,return true or false
+  };
 
+  const findObject = (object, currentLocation) => {
     if (isObject(object) === true) {
-
-      console.log(Object.keys(object),"keylist")
-      // const {obKey:obValue} = object;
-      // console.log ({obKey:obValue})
       const objectArray = Object.entries(object);
-      console.log ({objectArray},"aaa")
-      console.log(total)
       objectArray.forEach((value) => {
-        // let parentName = [];
-
         const [childKey, childValue] = value;
         if (isObject(childValue) === true) {
-          // parentName.push(childKey)
-          findObject(childValue);
-        
+          findObject(childValue, childKey); //recusion
         } else {
           const childArray = [];
-        
+          childTitle.push(childKey);
           const objectLength = Number(`${childValue}`);
           for (let i = 1; i <= objectLength; i++) {
-            childArray.push({ childKey, i });
-            // console.log(childArray)
-          };
-                    // parentName = [];
-
-          totalArray.push(childArray)
+            childArray.push({ childKey, i, currentLocation });
+          }
+          totalArray.push(childArray); //totalArray is a new array can be mapped in jsx
         }
-      }
-      )
-      console.log (totalArray);
-      return totalArray.map((childArrayName) => (
-        <div className="body eyes">
+      });
+      return totalArray.map((childArrayName, index) => (
+        <div className="body">
           <div className="avatar_title">
-            <div className=" avatar_type">{childArrayName.length}</div>
+            <div className=" avatar_type">{childTitle[index]}</div>
           </div>
           <div className="avatar_item">
-          {childArrayName.map((individualItem) => {
-            return (
-              <div className="avatar_item_children"><img class = "image_thumbnail" src={`character/${individualItem.childKey}/${individualItem.i}.png`} alt=""></img></div>
-            )
-          })}
+
+            {childArrayName.map((individualItem) => {
+              if (individualItem.currentLocation !== undefined) {
+                return (
+                  <div className="avatar_item_children">
+                    <img
+
+                      // ref = {callRef}
+                      id={`${individualItem.childKey}${individualItem.i}`}
+                      onClick={() => {
+                        addComponent(
+                          `${individualItem.currentLocation}/${individualItem.childKey}`,
+                          `${individualItem.childKey}`,
+                          `${individualItem.i}`
+                        );
+                      }}
+                      className="image_thumbnail"
+                      src={`character/${individualItem.currentLocation}/${individualItem.childKey}/${individualItem.i}.png`}
+                      alt=""
+                    ></img>
+                  </div>
+                );
+              } else {
+                return (
+                  <div className="avatar_item_children">
+                    <img
+                      id={`${individualItem.childKey}${individualItem.i}`}
+                      onClick={() => {
+                        addComponent(
+                          //function returns avatarArray,another array deviated from object "total" => iterate with map to render random avatar.
+                          //need to find a way to reuse "totalArray" array after the function contained it could return only HTML body not the array value
+                          `${individualItem.childKey}`,
+                          `${individualItem.childKey}`,
+                          `${individualItem.i}`
+                        );
+                      }}
+                      className="image_thumbnail"
+                      src={`character/${individualItem.childKey}/${individualItem.i}.png`}
+                      alt=""
+                    ></img>
+                  </div>
+                );
+              }
+            })}
           </div>
         </div>
-      ))
-        }
-  
+      ));
     }
-     
-   
+  };
 
+  const newArrayForAvatar = [];
+  const renderArray = (object) => {
+    if (isObject(object) === true) {
+      const objectArray = Object.entries(object);
+      objectArray.forEach((value) => {
+        if (isObject(value[1]) === true) {
+          renderArray(value[1]);
+        } else {
+          newArrayForAvatar.push(value);
+        }
+      });
+    }
+  };
+  renderArray(total);
 
-  //
-  //
-  // const findObject = (object) => {
-  //   if (isObject(object) === true) {
-  //     const objectArray = Object.entries(object);
-  //     console.log(objectArray, "2");
+  const newRandomArray = newArrayForAvatar.map((smallObject) => {
+    if (
+      smallObject[0] === `earrings` ||
+      smallObject[0] === `glasses` ||
+      smallObject[0] === `hats` ||
+      smallObject[0] === `neckwear`
+    ) {
+      return {
+        avatar_location: `/accessories/${smallObject[0]}`,
+        avatar_part: `${smallObject[0]}`,
+        avatar_status: `${Math.ceil(Math.random() * (smallObject[1] + 10))}`,
+      };
+    } else if (
+      smallObject[0] === `layer_1` ||
+      smallObject[0] === `layer_2` ||
+      smallObject[0] === `layer_3`
+    ) {
+      return {
+        avatar_location: `/clothes/${smallObject[0]}`,
+        avatar_part: `${smallObject[0]}`,
+        avatar_status: `${Math.ceil(Math.random() * smallObject[1] + 1)}`,
+      };
+    } else {
+      return {
+        avatar_location: `${smallObject[0]}`,
+        avatar_part: `${smallObject[0]}`,
+        avatar_status: `${Math.ceil(Math.random() * smallObject[1])}`,
+      };
+    }
+  });
 
-  //     objectArray.forEach((value) => {
-  //       const [childKey, childValue] = value;
-  //       if (isObject(childValue) === true) {
-  //         parentName = [];
-  //         parentName.push(childKey);
-  //         console.log(parentName, "3");
-  //         findObject(childValue);
-  //       } else {
-          // const objectLength = ;
-          // console.log(objectLength);
-          // if (parentName.length > 0) {
-          //   parentName.push(childKey);
-          // }
+  window.onload = () => {
+    setAvatarArray([...newRandomArray]);
+  };
 
-          
-  //           return (
-  //             <div className="body eyes">
-  //               <div className="avatar_title">
-  //                 <div className=" avatar_type">{parentName}</div>
-  //               </div>
-  //               <div className="avatar_item">
-  //                 {() => {
-  //                   for (let i = 0; i < Number(`${childValue}`); i++) {
-  //                     return (
-  //                       <div className="avatar_item_children">
-  //                         <img
-  //                           className="image_thumbnail"
-  //                           src={`character/${parentName}/${i}.png`}
-  //                           alt=""
-  //                         ></img>
-  //                       </div>
-  //                     );
-  //                   }
-  //                 }}
-  //               </div>
-  //             </div>
-  //           );
-          
-  //         // console.log(parentName)
-  //       }
-  //     });
-  //   }
-  // };
-
-  // console.log(findObject(total));
-
-  // const childArray = [];
-  // const objectLength = Number(`${childValue}`);
-  // if (parentName === [] ) {
-  //   parentName.push(childKey)
-  // }
-  // for (let i = 1; i <= objectLength; i++) {
-  //   childArray.push({ parentName, childKey, i });
-  //   // console.log(childArray)
-  // };
-  // parentName = [];
-  // totalArray.push(childArray)
-
-  // return totalArray.map((childArrayName) =>
-  // (
-  //   <div className="body eyes">
-  //     <div className="avatar_title">
-  //       <div className=" avatar_type">{parentName}</div>
-  //     </div>
-  //     <div className="avatar_item">
-  //     {childArrayName.map((individualItem) => {
-  //       return (
-  //         <div className="avatar_item_children"><img class = "image_thumbnail" src={`character/${individualItem.childKey}/${individualItem.i}.png`} alt=""></img></div>
-  //       )
-  //     })}
-  //     </div>
-  //   </div>
-
-  // )
-
-  // )
-
-  // const findObject = (object) => {
-  //   if (isObject(object) === true) {
-  //     console.log(Object.keys(object))
-  //     // const {obKey:obValue} = object;
-  //     // console.log ({obKey:obValue})
-  //     const objectArray = Object.entries(object);
-  //     console.log ({objectArray},"aaa")
-  //     console.log(total)
-  //     objectArray.forEach((value) => {
-  //       const [childKey, childValue] = value;
-  //       if (isObject(childValue) === true) {
-  //         findObject(childValue);
-  //       } else {
-  //         const childArray = [];
-  //         const objectLength = Number(`${childValue}`);
-  //         for (let i = 1; i <= objectLength; i++) {
-  //           childArray.push({ childKey, i });
-  //           // console.log(childArray)
-  //         };
-  //         totalArray.push(childArray)
-  //       }
-  //     }
-  //     );
-  //   }
-  //     console.log (totalArray)
-  //   return totalArray.map((childArrayName) => (
-  //     <div className="body eyes">
-  //       <div className="avatar_title">
-  //         <div className=" avatar_type">{childArrayName.length}</div>
-  //       </div>
-  //       <div className="avatar_item">
-  //       {childArrayName.map((individualItem) => {
-  //         return (
-  //           <div className="avatar_item_children"><img class = "image_thumbnail" src={`character/${individualItem.childKey}/${individualItem.i}.png`} alt=""></img></div>
-  //         )
-  //       })}
-  //       </div>
-  //     </div>
-  //   ))
-  //     }
   return (
     <body2>
       <section className="section1">
-        <h1 className="header_title">Character Customization</h1>
-        <h1 className="header_title">{count}</h1>
+        <h1 className="header_title">Character <br></br>Customization</h1>
         <h1 className="header_title">{}</h1>
       </section>
       <section className="section2">
         <div className="headContainer_wrapper">
           <div className="headContainer">
             <div className="avatar">
-              <img
-                className="avatar_overlap_item"
-                src="character/body/1.png"
-                alt=""
-              ></img>
+              {avatarArray.map((avatarInput) => (
+                <img
+                  className={`avatar_overlap_${avatarInput.avatar_part}`}
+                  src={`character/${avatarInput.avatar_location}/${avatarInput.avatar_status}.png`}
+                  alt=""
+                ></img>
+              ))}
             </div>
-            <button onClick={increaseCount} className="button">
+            <button
+              onClick={() => {
+                setAvatarArray([...newRandomArray]);
+              }}
+              className="button"
+            >
               RANDOMIZE!
             </button>
           </div>
         </div>
-        <div className="bodyContainer_wrapper">
-        {findObject(total)}
-          <img
-            className="image_thumbnail"
-            src="character/body/1.png"
-            alt=""
-          ></img>
-        </div>
+        <div className="bodyContainer_wrapper">{findObject(total)}</div>
       </section>
     </body2>
   );
